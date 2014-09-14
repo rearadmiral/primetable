31e99ffa6bcabbbb0bff505b640c6f019fd1d2ae
Bill DePhillips <bill.dephillips@gmail.com>
2014-09-14 05:07:32 -0700

    simple trial division

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,7 +1,20 @@
 class Primes
 
   def self.contains?(potential)
-    return true if potential == 1
+    return false if [1,2].include?(potential)
+    test(potential)
+  end
+
+  private
+
+  def self.test(potential)
+    (2..potential-1).each do |potential_divisor|
+      if potential % potential_divisor == 0
+        puts "#{potential} is not prime because it is divisible by #{potential_divisor}"
+        return false
+      end
+    end
+    true
   end
 
 end
--- spec/primes_spec.rb
+++ spec/primes_spec.rb
@@ -2,8 +2,16 @@ require_relative '../lib/primes'
 
 describe Primes do
 
-  it "knows that 1 is prime" do
-    expect(Primes.contains?(1)).to be true
+  it "knows that 1 is not prime" do
+    expect(Primes.contains?(1)).to be false
+  end
+
+  it "knows that 2 is not prime" do
+    expect(Primes.contains?(2)).to be false
+  end
+
+  it "knows that 3 is prime" do
+    expect(Primes.contains?(3)).to be true
   end
 
 end
